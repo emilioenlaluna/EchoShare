7052cc9e66a96799d62e9a5846eb35b0cf2a7ed3
Emilio Luna <al244182@edu.uaa.mx>
2024-04-04 04:12:49 -0600

    Remove keys

--- echoshare/custom_azure.py
+++ echoshare/custom_azure.py
@@ -2,13 +2,13 @@ from storages.backends.azure_storage import AzureStorage
 import os
 
 class AzureMediaStorage(AzureStorage):
-    account_name ="echosharestorage"#os.getenv('AZURE_ACCOUNT_NAME') # Must be replaced by your <storage_account_name>
-    account_key = "CjbkS5CsVbse2xpVvgZc0OA27RBHXFfUf+o8hp7zkozKX/knk9iXhErrg1FQLu7hqkaGNduGIY5Y+AStBOIicg=="#os.getenv('AZURE_ACCOUNT_KEY') # Must be replaced by your <storage_account_key>
+    account_name =os.getenv('AZURE_ACCOUNT_NAME') # Must be replaced by your <storage_account_name>
+    account_key =os.getenv('AZURE_ACCOUNT_KEY') # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = "echosharestorage"# os.getenv('AZURE_ACCOUNT_NAME') # Must be replaced by your storage_account_name
-    account_key = "CjbkS5CsVbse2xpVvgZc0OA27RBHXFfUf+o8hp7zkozKX/knk9iXhErrg1FQLu7hqkaGNduGIY5Y+AStBOIicg=="#os.getenv('AZURE_ACCOUNT_KEY') # Must be replaced by your <storage_account_key>
+    account_name = os.getenv('AZURE_ACCOUNT_NAME') # Must be replaced by your storage_account_name
+    account_key = os.getenv('AZURE_ACCOUNT_KEY') # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
